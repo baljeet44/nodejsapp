77191f1302c265ead51e8d131b849c84cd7f46ea
baljeet44 <baljeet.icfai2@gmail.com>
2024-01-01 19:49:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
-  res.write('This is TechMentor Tutorials, Please share and Subscribe and like my Videos'); //write a response to the client
+  res.write('This is TechMentor Tutorials. working on CI/CD pipeline'); //write a response to the client
   res.end(); //end the response
 }).listen(4000); //the server object listens on port 4000
